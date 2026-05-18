7c41f262dd9e48e8113c6a5bd5047066b6e750db
fyuo863 <2357645321@qq.com>
2026-05-18 22:40:32 +0800

    feat: 实现监听路由，引入charm.log包美化输出

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"FYUO_task_manager/pkg/log"
-	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"FYUO_task_manager/internal/config"
 	"FYUO_task_manager/internal/router"
@@ -14,8 +17,25 @@ func main() {
 	if err != nil {
 		log.Logger.Fatalf("[启动失败] 加载配置: %v", err)
 	}
-	log.Logger.Printf("[Config] 端口号: %s, 运行模式: %s", cfg.Server.Port, cfg.Server.Mode)
-	router.Setup(cfg.Server.Mode)
+	log.Logger.Info("[Config]", "端口号", cfg.Server.Port, "运行模式", cfg.Server.Mode)
+	r := router.Setup(cfg.Server.Mode)
 
-	defer func() { fmt.Println("安全退出占位") }()
+	srv := &http.Server{
+		Addr:    cfg.Server.Port,
+		Handler: r, // 将 Gin 引擎作为 HTTP Handler 传入
+	}
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT) //监测是否触发Ctrl+C
+
+	go func() { //在协程中启动Http服务
+		log.Logger.Info("[Server]", "HTTP 服务启动于", cfg.Server.Port)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Logger.Fatal("[Server]", "服务启动失败, err", err)
+		}
+	}()
+
+	sig := <-quit //阻塞Http协程直至触发退出
+	log.Logger.Info("[Server]", "接收到退出信号", sig)
+	defer func() { log.Logger.Error("安全退出占位") }()
 }
