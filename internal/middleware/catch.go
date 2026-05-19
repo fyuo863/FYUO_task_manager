f8057aa8e0d5f803e1aa87b8e0b6a43ad729fe6f
fyuo863 <2357645321@qq.com>
2026-05-19 12:45:57 +0800

    fix: 修复Catch()中间件发送业务响应的bug

--- internal/middleware/catch.go
+++ internal/middleware/catch.go
@@ -1,17 +1,15 @@
 package middleware
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func Catch() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		method := ctx.Request.Method
-		ctx.JSON(http.StatusOK, gin.H{
-			"Method": method,
-		})
+		// ctx.JSON(http.StatusOK, gin.H{
+		// 	"Method": method,
+		// })
 
 		ctx.Set("Method", method)
 		//log.Logger.Error("[Recovery]", "panic", method)
